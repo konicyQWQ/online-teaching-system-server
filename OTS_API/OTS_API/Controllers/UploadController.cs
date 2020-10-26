3ba2ef4d742205e5d7fcebbc83f4fedf63d24e4d
DWTwilight <65746072+DWTwilight@users.noreply.github.com>
2020-10-26 16:29:11 +0800

    1026#1

---
+++ OTS_API/OTS_API/Controllers/UploadController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OTS_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private readonly ILogger<UploadController> logger;
+
+        public UploadController(ILogger<UploadController> logger)
+        {
+            this.logger = logger;
+        }
+
+        
+    }
+}
