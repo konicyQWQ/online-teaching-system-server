1fea728c536a55940a76d63939413e064ae56add
DWTwilight <65746072+DWTwilight@users.noreply.github.com>
2020-10-26 16:50:14 +0800

    1026#2

--- OTS_API/OTS_API/Controllers/UploadController.cs
+++ OTS_API/OTS_API/Controllers/UploadController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -19,6 +20,29 @@ namespace OTS_API.Controllers
             this.logger = logger;
         }
 
-        
+        [HttpPost]
+        public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+                    logger.LogInformation("Filepath:" + filePath);
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            // Process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+
+            return Ok(new { count = files.Count, size });
+        }
     }
 }
