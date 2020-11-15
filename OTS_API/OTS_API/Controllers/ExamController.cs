393f843e2cb1b557e6820451b14ecfef6a3eead6
DWTwilight <65746072+DWTwilight@users.noreply.github.com>
2020-11-15 20:29:04 +0800

    1115#3

---
+++ OTS_API/OTS_API/Controllers/ExamController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OTS_API.Models;
+using OTS_API.Services;
+
+namespace OTS_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamController : ControllerBase
+    {
+        private readonly HomeworkExamService examService;
+        private readonly TokenService tokenService;
+
+        public ExamController(HomeworkExamService examService, TokenService tokenService)
+        {
+            this.examService = examService;
+            this.tokenService = tokenService;
+        }
+
+        [HttpPost]
+        public async Task<dynamic> OnAddExamAsync([FromForm] Exam exam, [FromForm] List<Question> questions, [FromForm] string token)
+        {
+            try
+            {
+                var t = await tokenService.GetTokenAsync(token);
+                if(t == null)
+                {
+                    throw new Exception("Token is Invalid!");
+                }
+                if(t.Role != UserRole.Admin)
+                {
+                    var courseRole = await examService.GetCourseRoleAsync(exam.CourseId, t.UserID);
+                    if(courseRole == UserRole.Student)
+                    {
+                        throw new Exception("Insufficient Authority!");
+                    }
+                }
+
+                await examService.AddExamAsync(exam, questions);
+                return new { Res = true };
+            }
+            catch (Exception e)
+            {
+                return new { Res = false, Error = e.Message };
+            }
+        }
+    }
+}
